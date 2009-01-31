2b72c6825aaebc149518d54ce99d3cbfdb391cbf
Longhao Luo <luolonghao@gmail.com>
2009-01-31 06:53:23 +0000

    Updated yuicompressor from 2.4.1 to 2.4.2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "Building kindeditor..."
-java -jar yuicompressor-2.4.1.jar --type js --charset utf-8 -v ./src/lang/zh_CN.js > ./build/kindeditor.js
-java -jar yuicompressor-2.4.1.jar --type js --charset utf-8 -v ./src/kindeditor-core.js >> ./build/kindeditor.js
-java -jar yuicompressor-2.4.1.jar --type js --charset utf-8 -v ./src/plugin-all.js >> ./build/kindeditor.js
+java -jar yuicompressor-2.4.2.jar --type js --charset utf-8 -v ./src/lang/zh_CN.js > ./build/kindeditor.js
+java -jar yuicompressor-2.4.2.jar --type js --charset utf-8 -v ./src/kindeditor-core.js >> ./build/kindeditor.js
+java -jar yuicompressor-2.4.2.jar --type js --charset utf-8 -v ./src/plugin-all.js >> ./build/kindeditor.js
 if [ -d ./build/plugins ]; then
 echo ""
 else
