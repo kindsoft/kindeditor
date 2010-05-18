732b6c17cc2dbaabef20bf05979729e0fb2c6b81
Longhao Luo <luolonghao@gmail.com>
2010-05-18 04:14:14 +0000

    [trunk] Modified php example

--- php/demo.php
+++ php/demo.php
@@ -1,25 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>KindEditor 3.0</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link href="../examples/index.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<div class="ke-content">
 <?php
-	if (get_magic_quotes_gpc()) {
-		echo(stripslashes($_POST['content']));
-	} else {
-		echo($_POST['content']);
+	$htmlData = '';
+	if (!empty($_POST['content1'])) {
+		if (get_magic_quotes_gpc()) {
+			$htmlData = stripslashes($_POST['content1']);
+		} else {
+			$htmlData = $_POST['content1'];
+		}
 	}
 ?>
-</div>
-<center>
-<br />
-<br />
-<input type="button" value="后退" onclick="javascript:history.back();" />
-</center>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>KindEditor ASP</title>
+	<script charset="utf-8" src="../kindeditor.js"></script>
+	<script>
+		KE.show({
+			id : 'content1',
+			imageUploadJson : '../../php/upload_json.php',
+			fileManagerJson : '../../php/file_manager_json.php',
+			allowFileManager : true,
+			afterCreate : function(id) {
+				KE.event.ctrl(document, 13, function() {
+					KE.util.setData(id);
+					document.forms['example'].submit();
+				});
+				KE.event.ctrl(KE.g[id].iframeDoc, 13, function() {
+					KE.util.setData(id);
+					document.forms['example'].submit();
+				});
+			}
+		});
+	</script>
+</head>
+<body>
+	<?php echo $htmlData; ?>
+	<form name="example" method="post" action="demo.php">
+		<textarea id="content1" name="content1" cols="100" rows="8" style="width:700px;height:200px;visibility:hidden;"><?php echo htmlspecialchars($htmlData); ?></textarea>
+		<input type="submit" name="button" value="提交内容" /> (提交快捷键: Ctrl + Enter)
+	</form>
 </body>
 </html>
 
