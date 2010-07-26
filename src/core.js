20fca6d4b79ebcb143b903a09b2d796296fccafe
Longhao Luo <luolonghao@gmail.com>
2010-07-26 06:42:03 +0000

    [trunk] Fixed issue #127

--- src/core.js
+++ src/core.js
@@ -1388,6 +1388,11 @@ KE.util = {
 		);
 	},
 	parseJson : function (text) {
+		//extract JSON string
+		var match;
+		if ((match = /\{[\s\S]*\}|\[[\s\S]*\]/.exec(text))) {
+			text = match[0];
+		}
 		var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 		cx.lastIndex = 0;
 		if (cx.test(text)) {
