0df21f32e4da9d1eac4201b0c4e954e1e8407722
Longhao Luo <luolonghao@gmail.com>
2011-10-29 14:40:32 +0000

    [trunk] remove console.log

--- kindeditor.js
+++ kindeditor.js
@@ -2712,7 +2712,6 @@ _extend(KCmd, {
 		if (needSplit) {
 			var dummy = doc.createElement('span');
 			range.cloneRange().collapse(!isStart).insertNode(dummy);
-			console.log(range.html());
 			if (isStart) {
 				tempRange.setStartBefore(parent.firstChild).setEnd(node, pos);
 			} else {
