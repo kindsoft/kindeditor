5366eaea2ff5d9427af722a1d7cb64bd72ce4df1
Longhao Luo <luolonghao@gmail.com>
2011-11-03 03:41:38 +0000

    [trunk] use jQuery ready when jQuery was loaded.

--- kindeditor.js
+++ kindeditor.js
@@ -5,7 +5,7 @@
 * @author Roddy <luolonghao@gmail.com>
 * @website http://www.kindsoft.net/
 * @licence http://www.kindsoft.net/license.php
-* @version 4.0.2 (2011-11-02)
+* @version 4.0.2 (2011-11-03)
 *******************************************************************************/
 (function (window, undefined) {
 	if (window.KindEditor) {
@@ -17,7 +17,7 @@ if (!window.console) {
 if (!console.log) {
 	console.log = function () {};
 }
-var _VERSION = '4.0.2 (2011-11-02)',
+var _VERSION = '4.0.2 (2011-11-03)',
 	_ua = navigator.userAgent.toLowerCase(),
 	_IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
 	_GECKO = _ua.indexOf('gecko') > -1 && _ua.indexOf('khtml') == -1,
@@ -539,6 +539,10 @@ function _ctrl(el, key, fn) {
 	});
 }
 function _ready(fn) {
+	if (window.jQuery && _isFunction(jQuery)) {
+		jQuery(fn);
+		return;
+	}
 	var loaded = false;
 	function readyFunc() {
 		if (!loaded) {
