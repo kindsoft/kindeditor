e6e3b3334453b8a78689ec0fbfd0dd0cb7307b46
Longhao Luo <luolonghao@gmail.com>
2012-01-15 14:45:51 +0000

    [trunk] update changelog

--- kindeditor.js
+++ kindeditor.js
@@ -5,7 +5,7 @@
 * @author Roddy <luolonghao@gmail.com>
 * @website http://www.kindsoft.net/
 * @licence http://www.kindsoft.net/license.php
-* @version 4.0.5 (2012-01-14)
+* @version 4.0.5 (2012-01-15)
 *******************************************************************************/
 (function (window, undefined) {
 	if (window.KindEditor) {
@@ -17,7 +17,7 @@ if (!window.console) {
 if (!console.log) {
 	console.log = function () {};
 }
-var _VERSION = '4.0.5 (2012-01-14)',
+var _VERSION = '4.0.5 (2012-01-15)',
 	_ua = navigator.userAgent.toLowerCase(),
 	_IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
 	_GECKO = _ua.indexOf('gecko') > -1 && _ua.indexOf('khtml') == -1,
