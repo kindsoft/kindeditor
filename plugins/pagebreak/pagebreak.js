6e8ad8e5876b03a6214ece2e742ca079422bb178
Longhao Luo <luolonghao@gmail.com>
2012-10-13 20:08:05 +0800

    Fix issue #38

--- plugins/pagebreak/pagebreak.js
+++ plugins/pagebreak/pagebreak.js
@@ -8,14 +8,17 @@
 *******************************************************************************/
 
 KindEditor.plugin('pagebreak', function(K) {
-	var self = this, name = 'pagebreak';
+	var self = this;
+	var name = 'pagebreak';
+	var pagebreakHtml = K.undef(self.pagebreakHtml, '<hr style="page-break-after: always;" class="ke-pagebreak" />');
+
 	self.clickToolbar(name, function() {
 		var cmd = self.cmd, range = cmd.range;
 		self.focus();
 		range.enlarge(true);
 		cmd.split(true);
 		var tail = self.newlineTag == 'br' || K.WEBKIT ? '' : '<p id="__kindeditor_tail_tag__"></p>';
-		self.insertHtml('<hr style="page-break-after: always;" class="ke-pagebreak" />' + tail);
+		self.insertHtml(pagebreakHtml + tail);
 		if (tail !== '') {
 			var p = K('#__kindeditor_tail_tag__', self.edit.doc);
 			range.selectNodeContents(p[0]);
